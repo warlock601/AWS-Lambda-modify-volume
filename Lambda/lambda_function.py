346523b001604570bd737112ed8904b0a42100cd
Vivek Raj <vr32288@gmail.com>
2023-10-22 13:21:31 +0530

    Updated lambda_function.py

--- Lambda/lambda_function.py
+++ Lambda/lambda_function.py
@@ -1,1 +1,23 @@
+import boto3
 
+def getvolumeidfrom_arn(volume_arn):
+    #split the ARN using colon :
+    arn_parts = volume_arn.split(':')
+    #the volume ID is the last part of the ARN after the 'volume/' prefix
+    volume_id = arn_parts[-1].split('/')[-1]
+    return volume_id
+
+def lambda_handler(event, context):
+    
+    
+    volume_arn = event['resources'][0]
+    volume_id = getvolumeidfrom_arn(volume_arn)
+    
+    ec2_client = boto3.client('ec2')
+
+    response = ec2_client.modify_volume(
+        VolumeId = volume_id,
+        VolumeType = 'gp3',
+    )
+
+    
